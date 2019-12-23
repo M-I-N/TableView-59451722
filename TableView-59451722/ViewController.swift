7661e8824d121616bfe4815a49c95b0b9278c020
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-12-23 14:26:08 +0600

    Initial Commit

---
+++ TableView-59451722/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView-59451722
+//
+//  Created by Mufakkharul Islam Nayem on 12/23/19.
+//  Copyright © 2019 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
